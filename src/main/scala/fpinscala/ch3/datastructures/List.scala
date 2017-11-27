9fdac993ee66776ab82d368ca16f7d74f0e4e74c
Ondrej Cerny <ondrej.cerny@oracle.com>
2017-11-27 18:28:50 +0100

    list exercises up to 3.15

--- src/main/scala/fpinscala/ch3/datastructures/List.scala
+++ src/main/scala/fpinscala/ch3/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.ch3.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -17,9 +19,17 @@ object List {
     case Cons(x,xs) => x * product(xs)
   }
 
-  def apply[A](as: A*): List[A] =
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
+  // The original apply was not tail recursive - thus unable to test tail recursivity of fold right in exercise 3.10
+  // def apply[A](as: A*): List[A] =
+  //    if (as.isEmpty) Nil
+  //    else Cons(as.head, apply(as.tail: _*))
+  def apply[A](as: A*): List[A] = {
+    @tailrec
+    def loop(acc: List[A], as: Seq[A]): List[A] =
+      if (as.isEmpty) acc
+      else loop(Cons(as.head, acc), as.tail)
+    loop(Nil, as.reverse)
+  }
 
   //exercise_3_1
   val x = List(1,2,3,4,5) match {
@@ -43,6 +53,7 @@ object List {
   }
 
   //exercise_3_4
+  @tailrec
   def drop[A](l: List[A], n: Int): List[A] = l match {
     case Nil => Nil
     case Cons(_, t) if n > 0 => drop(t, n-1)
@@ -50,6 +61,7 @@ object List {
   }
 
   //exercise_3_5
+  @tailrec
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Nil => Nil
     case Cons(h, t) if f(h) => dropWhile(t, f)
@@ -70,7 +82,7 @@ object List {
 
   //exercise_3_7
   def product_with_foldRight(ds: List[Double]): Double = foldRight(ds, 1.0)(_ * _)
-  // short circuiting not possible, would need extra short circuit condition to fold right
+  // short circuiting not possible, would need extra short circuit condition to fold right - or lazy structure like streams
 
   //exercise_3_8
   def selfFoldRight = foldRight(List(1, 2, 3), Nil:List[Int])(Cons(_, _))
@@ -78,5 +90,32 @@ object List {
   //exercise_3_9
   def length[A](l: List[A]): Int = foldRight(l, 0)((_, x) => x + 1)
 
-  //exercise_3_10 <- page 40
+  //exercise_3_10
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+    @tailrec
+    def loop(acc: B, as: List[A]): B = as match {
+      case Nil => acc
+      case Cons(h, t) => loop(f(acc, h), t)
+    }
+    loop(z, as)
+  }
+
+  //exercise_3_11
+  def lenFL[A](l: List[A]): Int = foldLeft(l, 0)((x, _) => x + 1)
+  def sumFL(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
+  def productFL(l: List[Double]): Double = foldLeft(l, 1.0)(_ * _)
+
+  //exercise_3_12
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, Nil:List[A])((acc, h) => Cons(h, acc))
+
+  // exercise_3_13
+  def foldRight2[A, B](l: List[A], z: B)(f: (A, B) => B): B = foldLeft(reverse(l), z)((a, b) => f(b, a))
+
+  // exercise_3_14
+  def append[A](l: List[A], e: A): List[A] = foldRight2(l, List(e))(Cons(_, _))
+
+  // exercise_3_15
+  def flatten[A](l: List[List[A]]): List[A] = reverse(foldLeft(l, Nil:List[A])((acc, as) => foldLeft(as, acc)((t, h) => Cons(h, t))))
+
+  // exercise_3_16 (page 42)
 }
--- src/test/scala/fpinscala/ch3/datastructures/ListSuite.scala
+++ src/test/scala/fpinscala/ch3/datastructures/ListSuite.scala
@@ -8,9 +8,68 @@ import fpinscala.ch3.datastructures.List._
 @RunWith(classOf[JUnitRunner])
 class ListSuite extends FunSuite {
 
-  // TODO - add tests of previous exercises too
+  // TODO - add tests of other exercises too
+
+  test("pattern matching") {
+    assert(List.x === 3)
+  }
 
   test("length") {
     assert(length(List(1, 2, 3)) === 3)
+  }
+
+  test("apply") {
+    assert(List(1, 2, 3) === Cons(1, Cons(2, Cons(3, Nil))))
+  }
+
+  test("foldRight not tail recursive") {
+    intercept[StackOverflowError] {
+      foldRight(List[Int](Stream.fill(10000)(1):_*), 0)(_ + _)
+    }
+  }
+
+  test("foldLeft is tail recursive") {
+    assert(foldLeft(List[Int](Stream.fill(10000)(1):_*), 0)(_ + _) === 10000)
+  }
+
+  test("lenFL") {
+    assert(lenFL(List(1, 2, 3, 4, 5)) == 5)
+  }
+
+  test("lenFL empty") {
+    assert(lenFL(List()) == 0)
+  }
+
+  test("sumFL") {
+    assert(sumFL(List(1, 2, 3)) == 6)
+  }
+
+  test("sumFL empty") {
+    assert(sumFL(List()) == 0)
+  }
+
+  test("productFL") {
+    assert(productFL(List(1.0, 2.0, 3.0)) == 6.0)
+  }
+
+  test("productFL empty") {
+    assert(productFL(List()) == 1.0)
+  }
+
+  test("reverse List(1, 2, 3) == List(3, 2, 1)") {
+    assert(reverse(List(1, 2, 3)) === List(3, 2, 1))
+  }
+
+  test("foldRight2 List(1, 2, 3), 0, _ -_") {
+    assert(foldRight2(List(1, 2, 3), 0)(_ - _) === foldRight(List(1, 2, 3), 0)(_ - _))
+  }
+
+  test("append List(1, 2, 3) 4 is List(1, 2, 3, 4)") {
+    assert(append(List(1, 2, 3), 4) === List(1, 2, 3, 4))
+  }
+
+
+  test("flatten List(List(1, 2), List(3, 4), List(5, 6)) ia List(1, 2, 3, 4, 5, 6)") {
+    assert(flatten(List(List(1, 2), List(3, 4), List(5, 6))) === List(1, 2, 3, 4, 5, 6))
   }
 }
